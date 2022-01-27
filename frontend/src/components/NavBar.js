abffb12f7f337126102febae319629948f952de5
Brian Glaws <glaws.brian@gmail.com>
2022-01-27 11:52:10 -0500

    added search button image to navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import logo from "../img/logo.jpg"
+//import searchButton from "../img/searchButton.js"
 
 export default function NavBar(props){
     const [lookUp,setLookUp] = React.useState("")
@@ -21,14 +21,17 @@ export default function NavBar(props){
             </Link>
             <Link to="/" className="HomePage"><h1 className="nav-title">TTPS Market</h1></Link>
             <div className="nav-links">
+                <input className="nav-search" type="text" placeholder="search item" value ={lookUp}onChange={SearchItem}></input>
+                <Link to="/search" className="search-link" state={{item:lookUp}}>
+                    <img className="search-button" src="https://static.thenounproject.com/png/424965-200.png"></img>
+                </Link>
                 {hasLogin && <span className="user-links">
                 <Link to ="/userprofile" className="userprofile-link">user</Link>
                 <Link to ="/order" className="order-link">order</Link>
                 <Link to ="/shoppingcart" className="shoppingcart-link">cart</Link>
                 <button onClick={()=>props.setlogin(false)}>Log out</button>
                 </span>}
-                <input className="nav-search" type="text" placeholder="search item" value ={lookUp}onChange={SearchItem}></input>
-                <Link to="/search" className="search-link" state={{item:lookUp}}>Search</Link>
+                
                 {!hasLogin && <Link to ="/login" className="login-link">login</Link> }
             </div>
         </nav>
