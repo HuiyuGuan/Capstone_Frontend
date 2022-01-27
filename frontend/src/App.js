08511301a081ce0c67da4d802c78543529988078
bohuinong <bohuinong@yahoo.com>
2022-01-27 17:31:40 -0500

    bug the login and the db not connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react"
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import { HashRouter,Route, Routes} from "react-router-dom"
 import axios from "axios"
 import Layout from "./components/Layout"
@@ -19,32 +19,38 @@ import AddProduct from './components/AddProduct';
 
 
 function App() {
-  const [hasLogin,setHasLogin] = useState(false)
+  const [user,setUser] = useState([])
 
-  function setlogin(value){
-    setHasLogin(value)
+  function setlogin(array){
   }
 
-  async function fetchuser(){
-    await axios.get("https://ttpsellit.herokuapp.com/users")
+  async function fetchUser(){
+    const users = await axios.get("https://ttpsellit.herokuapp.com/users")
+    if(users)
+      setUser(users.data)
   }
+
+  useEffect(() => {
+    fetchUser()
+    console.log(user)
+  }, [])
 
   return (
     <div className="App">
       <HashRouter>
         <Routes>
-          <Route exact path = "/" element={<Layout loginStatus={hasLogin} setlogin={setlogin}/>} >
+          <Route exact path = "/" element={<Layout user={user} setlogin={setlogin}/>} >
             <Route index element={<Home />} />
-            <Route path = "/login" element={<Login loginStatus={hasLogin} setlogin={setlogin}/>}/>
+            <Route path = "/login" element={<Login user={user} setlogin={setlogin}/>}/>
             <Route path = "/signup" element={<Signup />} />
             <Route path = "/search" element={<Search />} />
             <Route path = "/addproduct" element={<AddProduct />} />
-            <Route path = "/user" element={<User loginStatus={hasLogin}/>} />
-            <Route path = "/user/profile"element={<UserProfile loginStatus={hasLogin}/>} />
-            <Route path = "/user/sale" element={<UserSale loginStatus={hasLogin}/>} />
-            <Route path = "/user/feedback" element={<UserFeedback loginStatus={hasLogin} />} />
-            <Route path = "/order"element={<Orders loginStatus={hasLogin}/>} />
-            <Route path = "/shoppingcart"element={<ShoppingCart loginStatus={hasLogin}/>} />
+            <Route path = "/user" element={<User username={user.username}/>} />
+            <Route path = "/user/profile"element={<UserProfile username={user.username}/>} />
+            <Route path = "/user/sale" element={<UserSale username={user.username}/>} />
+            <Route path = "/user/feedback" element={<UserFeedback username={user.username} />} />
+            <Route path = "/order"element={<Orders username={user.username}/>} />
+            <Route path = "/shoppingcart"element={<ShoppingCart username={user.username}/>} />
             <Route path = "/productprofile" element={<ProductProfile />} />
           </Route>
         </Routes>
