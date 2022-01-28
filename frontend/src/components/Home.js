401df558143358c5b1495da0ea4b231ebd91d464
Bglaws <glaws.brian@gmail.com>
2022-01-28 00:47:11 -0500

    added list of random items whenever homepage is refreshed

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,28 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import ProductCard from "./ProductCard"
+import axios from "axios"
 
 export default function Home(){
+    const [random, setRandom] = useState([]);
+
+    async function randomItems() {
+        const items = await axios.get("https://ttpsellit.herokuapp.com/items");
+        const arr = [];
+        if (items) {
+            for (let i = 0; i < items.length; i++) {
+                arr.push(items)
+            }
+            setRandom(arr);
+        }        
+    }
+
+    useEffect(() => {
+        randomItems();
+
+    },[])
+
+    const randomList = random.map(item => <ProductCard key = {item.item_id} item = {item}/>)
+
     return (
         <div className ="home">
             <h2>Trending</h2>
@@ -13,11 +34,9 @@ export default function Home(){
             </div>
             <h2>Random</h2>
             <div className="random">
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
+                {randomList}
             </div>
         </div>
+
     )
 }
