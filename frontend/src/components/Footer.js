11978f1baa9279a193aceb003134e2d96f15bc36
HuiyuGuan <70614312+HuiyuGuan@users.noreply.github.com>
2022-01-28 03:46:30 -0500

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return(
     <div className="footer" >
-          &copy; {new Date().getFullYear()} Copyright: <a href=""> Smarket.com </a>
+          &copy; {new Date().getFullYear()} Copyright: <a href="https://keen-bell-6b4c1a.netlify.app"> Smarket.com </a>
     </div>
     )
 }
