328ff4097e8e0a3a3ab27dd12932a5fc49b826b8
HuiyuGuan <70614312+HuiyuGuan@users.noreply.github.com>
2022-01-28 03:45:35 -0500

    Update AddProduct.js

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import User from "./User";
+//import User from "./User";
 
 export default function AddProduct(props){
     const [name,setName] = useState("")
